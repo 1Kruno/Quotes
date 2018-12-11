95552d22d3227f1f9cf9a86b00a08a28ff70e035
1Kruno <44295358+1Kruno@users.noreply.github.com>
2018-12-11 23:16:52 +0000

    Add files via upload

---
+++ QuotesApp.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package quotesapp;
+
+/**
+ *
+ * @author kruno
+ */
+public class QuotesApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        QuotesGUI myGUI = new QuotesGUI();
+        myGUI.setVisible(true);
+    }
+    
+}
